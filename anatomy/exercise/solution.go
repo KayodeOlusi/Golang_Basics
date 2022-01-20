3a03323008e50c13e3262b88e1e523671c147045
KayodeOusi <kayodeolusi@gmail.com>
2022-01-20 14:09:46 +0100

    learnt about different ways of printing text to the console

---
+++ anatomy/exercise/solution.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	fmt.Printf("Hi! My name is %s. I have lived in for %d years. They say the weather is amazing, which is %t.", "Kayode", 5, true)
+}
--- anatomy/practice.go
+++ anatomy/practice.go
@@ -1,7 +1,16 @@
-package main
+package main // every go program must have a main package
 
-import "fmt"
+import "fmt" // import any other library that is needed after the main package
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World") // Println() to print to the console and move to the next line
+	fmt.Print("Hello World\n") // Print() to print to the console and continue on the same line (\n for new line)
+	fmt.Printf("Hi, my name is %s. I'm %d years old", "Kayode", 21)
+	/*
+		Printf() takes two arguments.. the string to output and the values to replace them with.
+		 %s for a string,
+		 %d for a number
+	*/
 }
+
+// NOTE: a directory cannot have more than one main() function.
