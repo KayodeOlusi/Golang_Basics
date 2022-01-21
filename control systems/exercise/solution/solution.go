cf480435e4bde7db7b879b21c620ede2b9003832
KayodeOusi <kayodeolusi@gmail.com>
2022-01-21 17:35:44 +0100

    control-systems

--- control systems/exercise/solution/solution.go
+++ control systems/exercise/solution/solution.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	sentence := "It is Morning"	// declare the variable
+	sentence := "It is Morning" // declare the variable
 
-	for index, letter := range sentence {	// loop through the variable and check if the index of the letter is odd
-		if index % 2 != 0 {	
-			fmt.Println(string(letter))		// use string() to convert the number-type string to string-type
+	for index, letter := range sentence { // loop through the variable and check if the index of the letter is odd
+		if index%2 != 0 {
+			fmt.Println(string(letter)) // use string() to convert the number-type string to string-type
 		}
 	}
 }
 
-// once a value is declared and not used, an error is thown. Use "_" to ignore. 
+// once a value is declared and not used, an error is thown. Use "_" to ignore.
